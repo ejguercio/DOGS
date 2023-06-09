ca8684ab1e4870c81f45988ae5280d4ed67e1734
Edgar Juan Guercio <jguercio@live.com.ar>
2023-06-09 01:42:46 -0300

    mejorando filtros

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,27 +1,27 @@
 import Card from "../Card/Card";
 import css from "./CardsContainer.module.css"
 import { useSelector } from "react-redux";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const CardsContainer = () => {
     //estado local
     const [currentPage, setCurrentPage] = useState(1);
     //estado global
-    const filteredDogs = useSelector(state => state.filteredDogs);
-    
+    const dogsToRender = useSelector(state => state.dogsToRender);
+
     useEffect(() => {
-        setCurrentPage(1); // Reiniciar currentPage a 1 cuando filteredDogs cambie
+        setCurrentPage(1); // Reiniciar currentPage a 1 cuando dogsToRender cambie
         console.log("se disparo UseEffect")
-      }, [filteredDogs]);
+    }, [dogsToRender]);
 
     //variables necesarias para el paginado
     const ITEMS_PER_PAGE = 8;
     const indexLastItem = currentPage * ITEMS_PER_PAGE;
     const indexFirstItem = indexLastItem - ITEMS_PER_PAGE;
-    const totalPages = Math.ceil(filteredDogs.length / ITEMS_PER_PAGE);
-  
-    //items que va a ir mosrando el contenedor de cards
-    const currentItems = filteredDogs.slice(indexFirstItem, indexLastItem);
+    const totalPages = Math.ceil(dogsToRender.length / ITEMS_PER_PAGE);
+
+    //items que va a ir mostrando el contenedor de cards
+    const currentItems = dogsToRender.slice(indexFirstItem, indexLastItem);
 
     const handlePrevious = () => {
         (currentPage > 1) && setCurrentPage(currentPage - 1)
@@ -34,22 +34,23 @@ const CardsContainer = () => {
         <div >
             <div className={css.container} >
             {currentItems.map(dog => {
-                return <Card
-                    key={dog.id}// para uso interno de React, no la veremos
-                    id={dog.id}
-                    name={dog.name}
-                    temperament={dog.temperament}
-                    weight={dog.weight}
-                    image={dog.image}
-                />
-            })}
+                    return <Card
+                        key={dog.id}// para uso interno de React, no la veremos
+                        id={dog.id}
+                        name={dog.name}
+                        temperament={dog.temperament}
+                        weight={dog.weight}
+                        image={dog.image}
+                    />
+                })}   
+            </div>
+            {(dogsToRender.length > ITEMS_PER_PAGE) ? //renderizado condicional pedido en la presentacion
+                <div className={css.contPaged}>
+                    <label className={css.labelPage}>Page: {currentPage}</label>
+                    <button className={css.buttons} onClick={handlePrevious}>PREVIOUS</button>
+                    <button className={css.buttons} onClick={handleNext}>NEXT</button>
+                </div> : <></>}
 
-            </div>
-            <div className={css.contPaged}>
-                <label className={css.labelPage}>Page: {currentPage}</label>
-                <button className={css.buttons} onClick={handlePrevious}>PREVIOUS</button>
-                <button className={css.buttons} onClick={handleNext}>NEXT</button>
-            </div>
         </div>
     )
 };
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -3,8 +3,9 @@
 import { CLEAN_DETAIL, GET_DOGS, GET_DOGS_BY_NAME, GET_DOG_BY_ID, GET_TEMPERAMENTS, FILTER_DOGS, ORDER_DOGS } from "./actions-type";
 
 const initialState = {
-    dogs: [],
-    filteredDogs: [],
+    allDogs: [],
+    dogsByName: [],
+    dogsToRender: [], //el que renderiza siempre el componente
     dog: {},
     temperaments: []
 };
@@ -14,8 +15,8 @@ const reducer = (state = initialState, { type, payload }) => {
         case GET_DOGS:
             return {
                 ...state,
-                dogs: payload,
-                filteredDogs: payload
+                allDogs: payload,
+                dogsToRender: payload,
             }
         case GET_DOG_BY_ID:
             return {
@@ -35,27 +36,35 @@ const reducer = (state = initialState, { type, payload }) => {
         case GET_DOGS_BY_NAME:
             return {
                 ...state,
-                filteredDogs: payload
+                dogsByName: payload,
+                dogsToRender: payload
             }
         case FILTER_DOGS:
-            const filtered = (payload === "AllDogs") ? state.dogs //TODOS
-                            : (payload === "DbDogs")? state.dogs.filter(dog => dog.created === true) //DB DOGS
-                            : state.dogs.filter(dog => dog.created === false); //API DOGS
+            let filtered = [];
+
+            if (payload === "AllDogs") {
+                filtered = state.allDogs; //ALL DOGS
+                state.dogsByName = [];
+            } else { //si hay una busqueda por nombre la signo sino filtro all
+                const dogsToFilter = state.dogsByName.length !== 0 ? state.dogsByName : state.allDogs; 
+                filtered = (payload === "DbDogs") ? dogsToFilter.filter(dog => dog.created) //DB DOGS
+                                                : dogsToFilter.filter(dog => !dog.created); //API DOGS
+            }
             return {
                 ...state,
-                filteredDogs: filtered
-            }
+                dogsToRender: filtered
+            };
         case ORDER_DOGS:
-            let ordered = [...state.filteredDogs]
+            let ordered = [...state.dogsToRender]
             //trabajo con esa copia porque si uso el estado directo, el useEffect no me renderizaba los cambios en tiempo real
-            ordered = (payload === "OrderA")? ordered.sort((a, b) => a.name.localeCompare(b.name))
-                        :(payload === "OrderD")? ordered.sort((a, b) => b.name.localeCompare(a.name))
-                        :(payload === "OrderByWeightA")? ordered.sort((a, b) => a.maxWeight - b.maxWeight)
+            ordered = (payload === "OrderA") ? ordered.sort((a, b) => a.name.localeCompare(b.name))
+                : (payload === "OrderD") ? ordered.sort((a, b) => b.name.localeCompare(a.name))
+                    : (payload === "OrderByWeightA") ? ordered.sort((a, b) => a.maxWeight - b.maxWeight)
                         : ordered.sort((a, b) => b.maxWeight - a.maxWeight)
-           return {
-             ...state,
-             filteredDogs: ordered
-           };       
+            return {
+                ...state,
+                dogsToRender: ordered
+            };
 
         default:
             return { ...state };
