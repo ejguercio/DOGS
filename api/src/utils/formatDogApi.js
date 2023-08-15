cc3d1a0c41ef18f9b36a26578696c61d2be787db
Edgar Juan Guercio <jguercio@live.com.ar>
2023-08-15 18:21:55 -0300

    adapte el back para renderizar las urls de imagenes porque el endpoint de la API Dogs cambio su estructura

--- api/src/utils/formatDogApi.js
+++ api/src/utils/formatDogApi.js
@@ -1,10 +1,12 @@
+const URL_IMAGES = "https://cdn2.thedogapi.com/images/";
+//const axios = require("axios");
 const formatDogApi = (arrDogs) =>
     arrDogs.map((dog) => {
         //creo un atributo de peso maximo para usar como ORDER en el FRONT
-            const matches = dog.weight.metric.match(/(\d+)\s*(?:-\s*(\d+)\s*)?(?:kg)?/);
-            let maxWeight = 0;
-            if (matches) maxWeight = parseInt(matches[2] || matches[1]);
-            
+        const matches = dog.weight.metric.match(/(\d+)\s*(?:-\s*(\d+)\s*)?(?:kg)?/);
+        let maxWeight = 0;
+        if (matches) maxWeight = parseInt(matches[2] || matches[1]);
+
         return {
             id: dog.id,
             name: dog.name,
@@ -12,7 +14,7 @@ const formatDogApi = (arrDogs) =>
             weight: `${dog.weight.metric} kg`,
             maxWeight: maxWeight,
             life_span: dog.life_span,
-            image: dog.image.url,
+            image: `${URL_IMAGES}${dog.reference_image_id}.jpg`,
             temperament: dog.temperament,
             created: false
         }
